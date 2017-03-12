07e58ba1636a7399ad784aad1c912e5bc43c8b6e
Ayoub serti <ayb.serti@gmail.com>
2017-03-12 12:06:54 +0000

    add all headers

---
+++ node-headers/node_header.h
@@ -0,0 +1,13 @@
+/*
+    @author Ayoub Serti
+    @email  ayb.serti@gmail.com
+    @abstract 
+            include this file if you want to use node internal headers
+*/
+
+#pragma once
+#define NODE_WANT_INTERNALS 1
+#include "node_wrap.h"
+
+
+//TODO: check node installed version against those headers
